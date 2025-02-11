a6c922978cbabafa1cc27dbb01d01e5dded52741
Somogyi Viktor <somogyi.viktor98@gmail.com>
2025-02-11 13:07:10 +0100

    encrypt and login added

--- server/model/User.js
+++ server/model/User.js
@@ -3,8 +3,15 @@ import mongoose from "mongoose";
 const {Schema, model} = mongoose
 
 const userSchema = new Schema ({
-    username: String,
-    password: String, 
+    username: 
+        {type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }, 
     tokens: Array,
     createdAt: Date
 })
