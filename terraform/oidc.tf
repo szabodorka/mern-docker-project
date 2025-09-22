9f420caf8adf795a686595c18632a9c526758355
szabodorka <szabodorkam@gmail.com>
2025-09-22 10:57:30 +0200

    feat(oidc): add GitHub OIDC provider and role for CI deployment

---
+++ terraform/oidc.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list   = ["sts.amazonaws.com"]
+  thumbprint_list  = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "gh_oidc_trust" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:OWNER/REPO:ref:refs/heads/main",
+        "repo:OWNER/REPO:ref:refs/heads/feature/infra-terraform-setup"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "gh_actions" {
+  name               = "gh-actions-mern-docker"
+  assume_role_policy = data.aws_iam_policy_document.gh_oidc_trust.json
+}
+
+resource "aws_iam_role_policy_attachment" "gh_admin" {
+  role       = aws_iam_role.gh_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "github_role_arn" {
+  value = aws_iam_role.gh_actions.arn
+}
