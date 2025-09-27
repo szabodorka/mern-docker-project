44cd435801e738540b70ddd2e6d2ae0c7881a7c8
szabodorka <szabodorkam@gmail.com>
2025-09-27 14:30:22 +0200

    feat(controller): create coins controller layer with Global, Markets and Coin route handlers

---
+++ backend/controllers/coins.controller.js
@@ -0,0 +1,60 @@
+import { config } from "../config/config.js";
+
+export async function Global(_req, res) {
+  try {
+    const response = await fetch("https://api.coingecko.com/api/v3/global", {
+      headers: { "x-cg-demo-api-key": config.coinGeckoKey },
+    });
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+    const data = await response.json();
+    res.json(data.data);
+  } catch (e) {
+    console.error("Error fetching data", e);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export async function Markets(_req, res) {
+  try {
+    const response = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1",
+      {
+        headers: {
+          "x-cg-demo-api-key": config.coinGeckoKey,
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    console.error("Error fetching crypto data", e);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export async function Coin(req, res) {
+  try {
+    const { id } = req.params;
+    const response = await fetch(
+      `https://api.coingecko.com/api/v3/coins/${id}`,
+      {
+        headers: {
+          "x-cg-demo-api-key": config.coinGeckoKey,
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    console.error("Error fetching selected token", e);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
